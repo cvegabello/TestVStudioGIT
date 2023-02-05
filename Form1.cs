595921959330e6e7dad4b0a419ed13076ce4a505
Carlos Vega <carlosvegabello@gmail.com>
2023-02-05 13:56:11 -0500

    Hola mundo by Josh

--- Form1.cs
+++ Form1.cs
@@ -10,12 +10,9 @@ namespace TestVStudioGIT
         private void button1_Click(object sender, EventArgs e)
         {
 
-            MessageBox.Show("Hello World");
+            
             MessageBox.Show("Hola mundo");
-            MessageBox.Show("Hello World my friend");
-            MessageBox.Show("Hello World From Josh");
-
-
+            
         }
     }
 }
