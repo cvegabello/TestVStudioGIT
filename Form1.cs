a39b861aadaf2e44727f17f467100c0dab533a45
Carlos Vega <carlosvegabello@gmail.com>
2023-02-05 13:27:58 -0500

    Hola mundo by Car On Carlos Branch

--- Form1.cs
+++ Form1.cs
@@ -10,7 +10,8 @@ namespace TestVStudioGIT
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Hello World");
-            
+            MessageBox.Show("Hola mundo");
+
 
         }
     }
