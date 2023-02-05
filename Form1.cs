978677ca3aeb2b0202138a770b609ce893616c3c
Carlos Vega <carlosvegabello@gmail.com>
2023-02-05 16:43:12 -0500

    W2

--- Form1.cs
+++ Form1.cs
@@ -15,36 +15,7 @@ namespace TestVStudioGIT
             
             MessageBox.Show("Hello World1");
             MessageBox.Show("Hello World2");
-            MessageBox.Show("Hello World3");
-            MessageBox.Show("Hello World4");
-            MessageBox.Show("Hello World5");
-            MessageBox.Show("Hello World6");
-            MessageBox.Show("Hello World7");
-            MessageBox.Show("Hello World8");
-            MessageBox.Show("Hello World9");
-            MessageBox.Show("Hello World10");
-            MessageBox.Show("Hello World11");
-            MessageBox.Show("Hello World12");
-            MessageBox.Show("Hello World13");
-            MessageBox.Show("Hello World14");
-            MessageBox.Show("Hello World15");
-            MessageBox.Show("Hello World16");
-            MessageBox.Show("Hello World17");
-            MessageBox.Show("Hello World18");
-            MessageBox.Show("Hello World19");
-            MessageBox.Show("Hello World20");
-            MessageBox.Show("Hello World21");
-            MessageBox.Show("Hello World30");
-            MessageBox.Show("Hello World31");
-            MessageBox.Show("Hello World32");
-            MessageBox.Show("Hello World33");
-            MessageBox.Show("Hello World34");
-            MessageBox.Show("Hello World35");
-            MessageBox.Show("Hello World36");
-            MessageBox.Show("SOY EL MEJOR");
-            MessageBox.Show("SUPERMAN");
-            MessageBox.Show("SOY EL PEOR");
-            MessageBox.Show("SOY mas o menos");
+            
         }
     }
 }
