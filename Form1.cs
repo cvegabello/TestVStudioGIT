c7d75b9be1cf8b0cf4df411d00b93e780b5d93c4
Carlos Vega <carlosvegabello@gmail.com>
2023-02-04 19:39:31 -0500

    Add Hola Mundo by Josh

--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,7 @@ namespace TestVStudioGIT
             MessageBox.Show("Hello World my friend");
             MessageBox.Show("Hello World From Carlos");
             MessageBox.Show("Hello World to everywhere");
+            MessageBox.Show("Hola mundo");
 
         }
     }
